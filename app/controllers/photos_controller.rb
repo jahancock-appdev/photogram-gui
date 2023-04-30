4d86b385ea72f3bd1400f23927317b3f3116d108
Jordan Hancock <jhancock@chicagobooth.edu>
2023-04-30 21:12:12 +0000

    Delete photo completed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,27 @@ class PhotosController < ApplicationController
 
     render({ :template => "photos_templates/index.html.erb"})
   end
+
+  def show
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id})
+    
+    @the_photo = matching_photos.first
+
+    render({ :template => "photos_templates/show.html.erb"})
+  end
+
+  def delete
+    the_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => the_id})
+
+    the_photo = matching_photos.first
+    the_photo.destroy
+
+    redirect_to("/photos")
+
+  end
+
 end
